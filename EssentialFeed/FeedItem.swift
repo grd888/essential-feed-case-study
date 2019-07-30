fff1cd3a6bff975f8154812ee595c050475cbefd
Greg Delgado <gdelgado@inovotek.com>
2019-07-31 05:27:47 +0800

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Greg Delgado on 7/31/19.
+//  Copyright © 2019 Greg Delgado. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    var id: UUID
+    var description: String?
+    var location: String?
+    var imageURL: URL
+}
