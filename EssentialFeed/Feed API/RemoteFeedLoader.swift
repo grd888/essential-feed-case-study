b21beb26db60c96f27778d0935a165cf32661376
Greg Delgado <gdelgado@inovotek.com>
2019-07-31 19:07:33 +0800

    Improve test names

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,31 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Greg Delgado on 7/31/19.
+//  Copyright © 2019 Greg Delgado. All rights reserved.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    
+    func get(from url: URL)
+    
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
+
+
