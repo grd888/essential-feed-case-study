9a9ed45b7a33792ed1f4a6fdeb7d303d81118f81
Greg Delgado <gdelgado@inovotek.com>
2019-08-17 14:46:43 +0800

    Move `RemoteFeedItem` to its own file

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -8,21 +8,12 @@
 
 import Foundation
 
-internal struct RemoteFeedItem: Decodable {
-    internal let id: UUID
-    internal let description: String?
-    internal let location: String?
-    internal let image: URL
-}
-
 internal final class FeedItemsMapper {
     private struct Root: Decodable {
         let items: [RemoteFeedItem]
         
     }
-    
-    
-    
+
     private static var OK_200: Int { return 200 }
     
     internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
